c6072bc80d93eb4aa5bf16836b296f5515333975
ananth-personal <106342157+ananth-personal@users.noreply.github.com>
2022-12-21 19:50:10 -0500

    Update variables.tf

--- tfe_workspace_setup/variables.tf
+++ tfe_workspace_setup/variables.tf
@@ -1,3 +1,9 @@
+variable "tfe_token" {
+  type        = string
+  description = "Self Explanatory"
+  sensitive   = true
+}
+
 variable "secret_value" {
   type        = string
   description = "Self Explanatory"
